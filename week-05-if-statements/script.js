82693b0a9147df46a6fb00139a193454aa3bfaca
armalik17 <98268974+armalik17@users.noreply.github.com>
2022-02-24 20:02:44 +0000

    Created my quiz!

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -6,33 +6,82 @@ function submitAnswerOne() {
   // Get the text from answer one's input element
   const answerOne = answerOneInput.value;
 
-  if(answerOne == 'cat') {
-    alert("That's right!");
+  if(answerOne == 'blue') {
+    alert("Yes! You know me so well.");
     hide('question-one');
     show('question-two');
-  } else if(answerOne == 'dog') {
-    alert('Close, but my favorite animal runs away from dogs.');
+  } else if(answerOne == 'green') {
+    alert('Close, but my favorite color is the color of the sky');
   } else {
-    alert('Wrong! Hint: My favorite animal meows.')
+    alert('Wrong! Hint: My favorite color is the opposite of orange')
   }
 }
 
-// When this function is called, it takes the user's answer to the second
-// question and uses if statements to check whether it's correct.
 function submitAnswerTwo() {
   // Get answer two's input element
   const answerTwoInput = document.getElementById('answer-two-input');
   // Get the number from answer two's input element
   const answerTwo = answerTwoInput.value;
 
-  if(answerTwo == 42) {
-    alert("That's right!");
+  if(answerTwo == 'basketball') {
+    alert(" Correct! GO VILLE");
     hide('question-two');
-    show('done');
-  } else if(answerTwo < 42) {
-    alert('The answer is higher.');
+    show('question-three');
+  } else if(answerTwo == 'tennis') {
+    alert('No, but it is played on a court.');
   } else {
-    alert('The answer is lower.')
+    alert('Wrong! Hint:There is an orange and black ball involved.')
+  }
+}
+
+function submitAnswerThree() {
+  // Get answer two's input element
+  const answerThreeInput = document.getElementById('answer-three-input');
+  // Get the number from answer two's input element
+  const answerThree = answerThreeInput.value;
+
+  if(answerThree == 3 ) {
+    alert("Yes. The perfect amount!");
+    hide('question-three');
+    show('question-four');
+  } else if(answerThree > 3 ) {
+    alert('Not that many!');
+  } else {
+    alert('Wrong! Hint: The anwser rhymes with tree')
+  }
+}
+
+function submitAnswerFour() {
+  // Get answer two's input element
+  const answerFourInput = document.getElementById('answer-four-input');
+  // Get the number from answer two's input element
+  const answerFour = answerFourInput.value;
+
+  if(answerFour == 'fall' ) {
+    alert("That's right!");
+    hide('question-four');
+    show('question-five');
+  } else if(answerFour == 'summer') {
+    alert('Almost');
+  } else {
+    alert('Wrong! Hint: The leaves change colors')
+  }
+}
+
+function submitAnswerFive() {
+  // Get answer two's input element
+  const answerFiveInput = document.getElementById('answer-five-input');
+  // Get the number from answer two's input element
+  const answerFive = answerFiveInput.value;
+
+  if(answerFive == 'hiking' || answerFive == 'yoga' || answerFive == 'gardening'|| answerFive == 'reading') {
+    alert("Yes! I love that.");
+    hide('question-five');
+    show('done');
+  } else if(answerFive == 'shopping') {
+    alert('No. I do not like to spend money');
+  } else {
+    alert('Not exactly. It is something you can do outside' )
   }
 }
 
