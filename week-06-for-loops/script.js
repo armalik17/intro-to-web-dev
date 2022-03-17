d259ec4d745417f56372b9834a36bb783199699c
armalik17 <98268974+armalik17@users.noreply.github.com>
2022-03-17 22:05:35 +0000

    Project #7

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -133,6 +133,12 @@ if (!commonWords.includes(word)) {
   }
 }
 
+let total = 0;
+for (let number = 1; number <= 10; number++) {
+  total += number;
+}
+console.log(total);
+
 // TODO: Define your own functions here!
 
 function displaySpeechStats() {
--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,7 +1,70 @@
+let message = 'VILLE TENNIS';
+let textY = 200;
+let ySpeed = 2; 
+let circleY = [];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 500);
+  for (let i = 0; i < 50; i++) {
+    circleY[i] = random(height);
+  
+  myCircle = new Circle(150, 150, 3, 5);
+ 
+  }
 }
 
 function draw() {
-  background(220);
+  background(100,165,70);
+
+  
+//Text// 
+  textSize(35);  
+  text(message , 80, textY);
+  
+  textY += ySpeed; 
+    if (textY > height || textY < 0 ) {
+      ySpeed *= -1;
+    }
+
+  
+// 50 Small Tennis Balls // 
+for (let i = 0; i < circleY.length; i++) {
+    let circleX = width * i / circleY.length;
+    circle(circleX, circleY[i], 30);
+
+    circleY[i]++;
+    
+    if (circleY[i] > height) {
+      circleY[i] = 0;
+    }
+  }
+  
+// One Large Tennis Ball// 
+
+  fill (200,400,40)
+  
+  myCircle.x += myCircle.xSpeed;
+  if (myCircle.x < 0 || myCircle.x > width) {
+    myCircle.xSpeed *= -1;
+  }
+
+  myCircle.y += myCircle.ySpeed;
+  if (myCircle.y < 0 || myCircle.y > height) {
+    myCircle.ySpeed *= -1;
+  }
+  
+  circle(myCircle.x, myCircle.y, 50);
+}
+
+let myCircle;
+
+class Circle {
+  constructor(x, y, xSpeed, ySpeed) {
+    this.x = x;
+    this.y = y;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+  }
+
 }
