aea46fa72f16dea56f4427f4e6a14225c7ec6bab
armalik17 <98268974+armalik17@users.noreply.github.com>
2022-03-31 23:40:56 +0000

    wiki project!

--- week-09-fetch/script.js
+++ week-09-fetch/script.js
@@ -1,6 +1,6 @@
 // Loads content for a Wikipedia article and displays it in the page.
 async function loadContent() {
-  const pageId = 'Cat';
+  const pageId = 'Nature';
   const response = await fetch(
     'https://en.wikipedia.org/w/api.php?' +
     'action=parse&formatversion=2&format=json&origin=*&page='
@@ -8,21 +8,61 @@ async function loadContent() {
   const json = await response.json();
   const article = json.parse;
 
-  // Get the links in the Wikipedia article, and display them in the page.
-  const linksElement = document.getElementById('links');
+//Categories from Nature page 
+
+let categories = json.parse.categories;
+
+let sortkeyElement = document.getElementById ('sortkey');
+
+for (let sort of categories) {
+  let liElement = document.createElement ('li'); 
+  let aElement = document.createElement ('a');
+  aElement.innerText = sort.category;
+  
+  liElement.appendChild(aElement);
+  sortkeyElement.appendChild(liElement);
+  }
+  console.log (article.links);
+  
+  
+// Languages with active links to different sites 
+
+let title = article.title;
+
+let titleElement = document.getElementById('title');
+  titleElement.innerText = title; 
+
+let langLinks = json.parse.langlinks;
+
+let linksElement = document.getElementById('langlinks');
+  
+for (let link of langLinks) {
+  let liElement = document.createElement ('li'); 
+  let aElement = document.createElement ('a');
+  aElement.innerText = link.title;
+  aElement.href = link.url;
+  
+  liElement.appendChild(aElement);
+  linksElement.appendChild(liElement);
+  }
+  console.log (article.links);
+
+  // Get the links in the Wikipedia article, and display them in         the page.
+  
+  const interalLinksElement = document.getElementById('interallinks');
   for (const link of article.links) {
     const linkElement = document.createElement('a');
+
     linkElement.href = 'https://en.wikipedia.org/wiki/' + link.title;
     linkElement.innerText = link.title;
 
     const liElement = document.createElement('li');
     liElement.appendChild(linkElement);
 
-    linksElement.appendChild(liElement);
+    interalLinksElement.appendChild(liElement);
   }
 
   loadImages(article.images);
-}
 
 // Takes an array of image file names, uses the Wikipedia API to get the full
 // URL for each one, and then displays them in the page.
@@ -50,5 +90,9 @@ async function loadImages(images) {
       const imageElement = document.createElement('img');
       imageElement.src = imageUrl;
       imagesContainer.appendChild(imageElement);
+    }
   }
 }
+  
+
+
